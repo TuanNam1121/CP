ad9e81b0a29e49fecf1b0c57ec585ec8dd29fec6
nam1 <namnguyen170681@gmail.com>
2025-08-24 22:56:28 +0700

    Hoan thanh ly thuyet buoi 1

--- C++/Ly_Thuyet/buoi1.cpp
+++ C++/Ly_Thuyet/buoi1.cpp
@@ -3,76 +3,68 @@ using namespace std;
 #define ll long long
 
 int main(){
-    pair : cấu trúc lưu đc 2 thằng đi kèm với nhau
-    pair<kiểu dữ liệu 1, kiểu dữ liệu 2>
-    pair<int,int> a = make_pair(100,200); gan makepair
-    a = {100,200}; gan truc tiep 
-    pair<int, int> b = a ; gan b = a và phải cùng 2 kiểu
-    
-    cout << a.first << "  " << a.second << endl; nhập pair
-    cin >> a.first;
-    cin >> a.second;
-    cout << a.first << " " << a.second << endl;
-    
-    mảng pair 
-    pair<int, int> arr[100];
-    
-    lưu hoành độ vào tung độ
-    int n ; cin >> n;
-    
-    for(int i = 0 ; i < n ; i++){
-        cin >> arr[i].first >> arr[i].second;
-    }
+    // int n, m; cin >> n >> m;
+    // vector<int> a(n), b(m);
+    // for(int i = 0; i < n ;i++){
+    //     cin >> a[i];
+    // }
 
-    for(int i = 0 ; i < n ; i++){
-        cout << arr[i].first << " " << arr[i].second << endl;
-    }
+    // for(int i = 0 ; i < m ; i++){
+    //     cin >> b[i];
+    // }
+
+    // vector<int> hop, giao;
     
-    pair<int,pair<int,int>> a;
-    cout << a.first << endl;
-    cout << a.second.first << " " << a.second.second << endl; // a.second la 1 pair
+    // int i,j; i = j = 0;
 
+    // while(i < n && j < m){
+    //     if(a[i] == b[j]){
+    //         hop.push_back(a[i]);
+    //         giao.push_back(a[i]);
+    //         i++; j++;
+    //     }
+    //     else if(a[i] > b[j]){
+    //         giao.push_back(b[j]);
+    //         j++;
+    //     }
+    //     else{
+    //         giao.push_back(a[i]);
+    //         i++;
+    //     }
+    // }
 
-    vector : 1 mảng động có thể thu hẹp và mở rộng tùy ý.tốc độ nhanh như mảng
+    // while(i < n){
+    //     hop.push_back(a[i]);
+    //     i++;
+    // }
+
+    // while(j < m){
+    //     hop.push_back(b[j]);
+    //     j++;
+    // }
+    // cout << "Hop cua 2 mang :" << endl;
+    // for(int i : hop) cout << i << " " ;
+    // cout << endl;
+    // cout << "Giao cua 2 mang :" << endl; 
+    // for(int i : giao) cout << i << " ";
+
+    // int n; cin >> n;
+    // vector<int> a(n);
+    // for(int i = 0 ; i < n ; i++){
+    //     cin >> a[i];
+    // }
+    // sort(a.begin(), a.end()); // sort tăng dần
+    // for(int i : a) cout << i << ' ';
+    // cout << endl;
+    // sort(a.begin(), a.end(), greater<int>()); // sort giảm dần
+    // for(int i : a) cout << i << " ";
+
     vector<int> a;
-    vector<int> v(10); // tạo sẵn cho mảng có 10 phần tử 
-    a.push_back(50); // -> đẩy 1 giá trị vào vector
-    a.push_back(100);
-    cout << a.size() << endl; // -> xem có bao nhiêu phần tử
-    a.push_back(200);
-    cout << a.size() << endl;
-
-    for(int i = 0 ; i < (int)a.size(); i++){
-        cout << a[i] << " ";
-    }
-    cout << endl;
-    for(int i = a.size() - 1 ; i >= 0; i--){
-        cout << a[i] << " ";
-    }
-    cout << endl;
-
-    // iterator
-    cout << *a.begin() << endl; // -> trỏ đến thằng đầu tiên
-    cout << *a.end() << endl; // -> trỏ đến null đánh dấu hết vector
-
-    for(vector<int>::iterator it = a.begin(); it != a.end(); ++it){
-        cout << *it << " " ;
+    int temp;
+    while(cin >> temp){
+        a.push_back(temp);
     }
 
-    // thay thế iterator = auto cho dễ cài đặt
-
-    for(auto it = a.begin(); it != a.end(); ++it){
-        cout << *it << " " ;
-    }
-    cout << endl;
-    // it + x sẽ tương đương với a[x] trong mảng
-    cout << *(a.begin() + 2);
-
-    int n;cin >>n;
-    vector<int> mang(n,100); // tạo 1 mảng có n phần tử và toàn bộ phần tử là 100
-
-    memset : memset(mảng cần gán, số gần gán (tuy nhiên memset chỉ gán đc 3 giá trị là -1 , 0 , 1), số lượng phần tử cần gán)
-    int mem[100];
-    memset(mem, -1, sizeof(mem));
-    for(int i : mem) cout << i << " " ;
+    for(int i : a) cout << i << " ";
+    
 }
